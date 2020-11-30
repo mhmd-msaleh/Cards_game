e06e7fdfc2e18b30967bcddfb2089d82fc1e83c1
Mohammed M.Saleh <64287070+mohammed1492@users.noreply.github.com>
2020-11-30 11:45:41 +0300

    Update Main.java

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -14,25 +14,21 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
 
+        //Creating ArrayList for Buttons
         ArrayList<Button> buttonsList = new ArrayList<>();
+        
         //Creating ArrayList and Loading images from "images" directory
         ArrayList<Image>  imagesList = new ArrayList<>();
         for(int i = 1; i <= 8; i ++ ) {
             String imageName = "file:images/"+i+".jpg";     //image path => images/num.jpg
             imagesList.add(new Image(imageName));           //add image to imagersLIst
-
-            buttonsList.add(new Button());
         }
+        
+        //Creating BorderPane to organize all components
         BorderPane borderPane = new BorderPane();
 
-
+        //Setting Scene and primaryStage
         Scene scene = new Scene(borderPane);
-
-
-        //kjlfdkjkdlfj/j
-        //jfkdlsfkdjl
-        //jldkjfkdjlsjkjlskdjkf
-        //Setting primaryStage
         primaryStage.setTitle("Memory Game ");
         primaryStage.setScene(scene);
         primaryStage.show();
